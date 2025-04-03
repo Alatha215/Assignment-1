364810f693aeffbb09103d4fa3d8f120ed9bc72b
BoikanyoMonama <st10467843@imconnect.edu.za>
2025-04-03 22:50:17 +0200

    first commit

---
+++ app/src/main/java/za/ac/iie/mealplan01/MainActivity.kt
@@ -0,0 +1,17 @@
+package za.ac.iie.mealplan01
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+
+        }
+    }
